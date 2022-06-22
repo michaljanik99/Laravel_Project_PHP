aa68e448a542c83730897148f27a66dc1234a59d
Michał Janik <mjanik45@gmail.com>
2022-06-22 18:19:04 +0200

    Add Test buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,19 +4,29 @@
 <div class="container">
     <div class="row">
         <div class="col s12 m4 l3">
-            <a href="/posty-wewnetrzne">
-                <div class="card-panel hoverable center">
-                    <h5>Posty wewnętrzne</h5>
-                </div>
-            </a>
-            <a href="/wydarzenia-wewnetrzne">
-                <div class="card-panel hoverable center">
-                    <h5>Wydarzenia wewnętrzne</h5>
-                </div>
-            </a>
             <a href="/users">
                 <div class="card-panel hoverable center">
                     <h5>Users</h5>
+                </div>
+            </a>
+            <a href="/priorityes">
+                <div class="card-panel hoverable center">
+                    <h5>Priorityes</h5>
+                </div>
+            </a>
+            <a href="/sprints">
+                <div class="card-panel hoverable center">
+                    <h5>Sprints</h5>
+                </div>
+            </a>
+            <a href="/tasks">
+                <div class="card-panel hoverable center">
+                    <h5>Tasks</h5>
+                </div>
+            </a>
+            <a href="/positions">
+                <div class="card-panel hoverable center">
+                    <h5>Positions</h5>
                 </div>
             </a>
         </div>
